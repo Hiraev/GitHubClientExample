9b1fc486560ea2cb79051941ad5a21d4274d6096
Malik <hiraev.github@gmail.com>
2020-07-20 16:58:55 +0300

    Add base class for Middleware

---
+++ app/src/main/kotlin/ru/khiraevmalik/githubclientexample/presentation/base/DisposableMiddleware.kt
@@ -0,0 +1,36 @@
+package ru.khiraevmalik.githubclientexample.presentation.base
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import ru.khiraevmalik.githubclientexample.mvi_no_reactive.Middleware
+
+abstract class DisposableMiddleware<A, E, S, N>(
+        job: Job = Job(),
+        dispatcher: CoroutineDispatcher = Dispatchers.Default
+) : Middleware<A, E, S, N>() where E : A {
+
+    private val scope = CoroutineScope(job + dispatcher)
+
+    override fun onDispose() {
+        scope.cancel()
+    }
+
+    fun effectOnMain(effect: E) {
+        scope.launch(Dispatchers.Main) {
+            effect(effect)
+        }
+    }
+
+    fun newsOnMain(news: N) {
+        scope.launch(Dispatchers.Main) {
+            news(news)
+        }
+    }
+
+    fun launch(block: suspend CoroutineScope.() -> Unit) = scope.launch(block = block)
+
+}
