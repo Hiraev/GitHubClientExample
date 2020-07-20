58f54bcc9525deb2fc23c79ebf6a07bb44235d71
Malik <hiraev.github@gmail.com>
2020-07-20 21:35:27 +0300

    Add view extensions to add system insets for status bar

--- app/src/main/kotlin/ru/khiraevmalik/githubclientexample/presentation/utils/ExtensionsView.kt
+++ app/src/main/kotlin/ru/khiraevmalik/githubclientexample/presentation/utils/ExtensionsView.kt
@@ -1,6 +1,9 @@
 package ru.khiraevmalik.githubclientexample.presentation.utils
 
+import android.graphics.Rect
 import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 fun View.visible(visible: Boolean) {
     this.visibility = if (visible) View.VISIBLE else View.GONE
@@ -11,3 +14,49 @@ fun View.visibleWithCheck(visible: Boolean) {
 }
 
 fun View.isVisible() = this.visibility == View.VISIBLE
+
+fun View.updatePaddings(
+        start: Int = paddingStart,
+        top: Int = paddingTop,
+        end: Int = paddingEnd,
+        bottom: Int = paddingBottom
+) {
+    this.setPadding(start, top, end, bottom)
+}
+
+fun View.addSystemTopPadding(targetView: View = this) {
+    doOnApplyWindowInsets { _, insets, initialPadding ->
+        targetView.updatePaddings(
+                top = initialPadding.top + insets.systemWindowInsetTop
+        )
+        insets.replaceSystemWindowInsets(
+                insets.systemWindowInsetLeft,
+                0,
+                insets.systemWindowInsetRight,
+                insets.systemWindowInsetBottom
+        )
+    }
+}
+
+fun View.doOnApplyWindowInsets(block: (View, insets: WindowInsetsCompat, initialPadding: Rect) -> WindowInsetsCompat) {
+    val initialPadding = Rect(paddingLeft, paddingTop, paddingRight, paddingBottom)
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        block(v, insets, initialPadding)
+    }
+    requestApplyInsetsWhenAttached()
+}
+
+private fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        ViewCompat.requestApplyInsets(this)
+    } else {
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                ViewCompat.requestApplyInsets(v)
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
