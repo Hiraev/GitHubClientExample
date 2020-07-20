6694fe6f2f4410d01f25259385d6822eb18c2c5c
Malik <hiraev.github@gmail.com>
2020-07-20 14:18:51 +0300

    Add di container

---
+++ di-container/src/main/kotlin/ru/khiraevmalik/githubclientexample/di_container/DiContainer.kt
@@ -0,0 +1,43 @@
+package ru.khiraevmalik.githubclientexample.di_container
+
+import ru.khiraevmalik.githubclientexample.data.MoshiFactory
+import ru.khiraevmalik.githubclientexample.data.OkHttpFactory
+import ru.khiraevmalik.githubclientexample.data.RetrofitFactory
+import ru.khiraevmalik.githubclientexample.data.repositories.GitHubCommitsRepositoryImpl
+import ru.khiraevmalik.githubclientexample.data.repositories.GitHubPublicReposRepositoryImpl
+import ru.khiraevmalik.githubclientexample.domain.interactors.GitHubCommitsInteractorImpl
+import ru.khiraevmalik.githubclientexample.domain.interactors.GitHubReposInteractorImpl
+import ru.khiraevmalik.githubclientexample.interactors.GitHubCommitsInteractor
+import ru.khiraevmalik.githubclientexample.interactors.GitHubReposInteractor
+
+object DiContainer {
+
+    private val moshi by lazy {
+        MoshiFactory.createMoshiForGitHubApi()
+    }
+
+    private val okHttpClient by lazy {
+        OkHttpFactory.createOkHttp()
+    }
+
+    private val retrofit by lazy {
+        RetrofitFactory.createForGitHub(moshi, okHttpClient)
+    }
+
+    private val gitHubApi by lazy {
+        RetrofitFactory.createGitHubApi(retrofit)
+    }
+
+    private val gitHubReposRepository by lazy {
+        GitHubPublicReposRepositoryImpl(gitHubApi)
+    }
+
+    private val gitHubCommitsRepository by lazy {
+        GitHubCommitsRepositoryImpl(gitHubApi)
+    }
+
+    fun provideCommitsInteractor(): GitHubCommitsInteractor = GitHubCommitsInteractorImpl(gitHubCommitsRepository)
+
+    fun provideReposInteractor(): GitHubReposInteractor = GitHubReposInteractorImpl(gitHubReposRepository)
+
+}
